53635d7a7779f38d15984c9755bd3e2ecaf59786
RadheSravan <paturisravan@gmail.com>
2016-02-29 20:19:46 +0530

    Java program to find the prime factors of a given number

---
+++ PrimeFactors.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class PrimeFactors
+{
+	public static void main(String args[])
+	{
+		long number = 0, factor = 2, input = 0;
+		HashSet<Long> set = new HashSet<Long>(); 
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the number to be prime factorized : ");
+		number = scanner.nextLong();
+		input = number;
+
+		while(number != 0)
+		{
+			if(number%factor != 0)
+			{
+				factor = factor + 1;
+			}
+			else
+			{
+				number = number/factor;
+				set.add(factor);					// Adding to a HashSet to prevent duplicate entries 
+
+				if(number == 1)
+				{
+					break;
+				}
+			}
+		}
+		
+		System.out.println("\nThe following are the " + set.size() + " prime factors of " + input + " : " );
+		for(Long factor : set)
+		{
+			System.out.print(factor + " ");
+		}
+		System.out.println("\n");
+	}
+}
